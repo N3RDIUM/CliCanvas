7bd9f94c6df3fca9b9d758d035da396ec395caad
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-07-24 20:22:20 +0530

    Better background

--- characters.py
+++ characters.py
@@ -1,4 +1,4 @@
-ASCII = "¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;\"^:,'.`"
+ASCII = "¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;\"^:,'.` "
 # reverse the string
 ASCII = ASCII[::-1]
 N_ASCII = len(ASCII)
